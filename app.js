4b7399377d81e733352f6c6c6a0cc8e1ee2a7f39
Samuel <samaraujo28@outlook.com>
2021-04-23 01:03:44 -0300

    Inserting new features and refacturing

--- app.js
+++ app.js
@@ -1,115 +1,146 @@
-const correctAnswers = ['B', 'D', 'A', 'D', 'C', 'A', 'C', 'B'];
-let shouldReloadPage = false;
-let score = 0;
+(() => {
+    const correctAnswers = ['B', 'D', 'A', 'D', 'C', 'A', 'C', 'B'];
+    const form = document.querySelector('form');
+    const popup = document.querySelector('.popup-wraper');
+    const popupTitle = document.querySelector('.popup-title');
+    const popupScore = document.querySelector('.popup-score');
+    const buttonForm = document.querySelector('button');
+    const inputs = document.querySelectorAll('input')
 
-const form = document.querySelector('form');
-const popup = document.querySelector('.popup-wraper');
-const popupTitle = document.querySelector('.popup-title');
-const popupParagraph = document.querySelector('.popup-paragraph');
-const button = document.querySelector('button');
+    let counter = 0;
+    let shouldReloadPage = false;
+    let score = 0;
 
-const highlightWrongUserAnswer = input => {
-    const verifyCheckedWrongAnswer = input.checked;
+    const getUserAnswerFeedback = (userAnswer, index) => {
+        const correctAnswer = {
+            highlight: 'highlight-right-answer',
+            textColor: 'text-success'
+        };
+    
+        const wrongAnswer = {
+            highlight: 'highlight-wrong-answer',
+            textColor: 'text-danger'
+        };
 
-    if (verifyCheckedWrongAnswer) {
-        const labelOfInput = input.parentElement
-        const divOfInput = labelOfInput.parentElement;
+        const isUserAnswerCorrect = userAnswer === correctAnswers[index];
 
-        divOfInput.classList.add('highlight-wrong-answer');
-    };
-}
-
-const showWrongAnswers = (radioNodeList, index) => {
-    const userAnswer = radioNodeList.value;
-    const verifyUserWrongAnswer = userAnswer !== correctAnswers[index];
-
-    if (verifyUserWrongAnswer) {
-        radioNodeList.forEach(highlightWrongUserAnswer);
-    };
-};
-
-const showPopup = (scoreFeedback) => {
-    const { title, paragraph, className } = scoreFeedback();
-
-    popup.style.display = 'block';
-
-    popupTitle.textContent = title;
-    popupTitle.classList.add(className);
-
-    popupParagraph.textContent = paragraph;
-};
-
-const closePopup = event => {
-    const classNameOfClickedElement = event.target.classList;
-    const classesNames = ['popup-close', 'popup-wraper'];
-
-    const shouldClosePopup = classesNames.some(
-        className => className === classNameOfClickedElement[0]
-    );
-
-    if (shouldClosePopup) {
-        popup.style.display = 'none';
-    };
-};
-
-const testScore = () => {
-    const isAGoodScore = score >= 4;
-
-    const goodScoreMessage = {
-        title: "Parabéns!",
-        paragraph: `Você acertou ${score} de 8 frases`,
-        className: 'text-success'
+        return isUserAnswerCorrect ? correctAnswer : wrongAnswer;
     };
 
-    const badScoreMessage = {
-        title: 'Que pena!',
-        paragraph: `Você acertou ${score} de 8 frases, 
-            precisa estudar um pouco mais!`,
-        className: 'text-danger'
+    const highlightCheckedInputs = (inputs) => {
+        inputs.forEach( input => {
+            const isInputChecked = input.checked;
+
+            if (!isInputChecked) {
+                return;
+            };
+    
+            const inputLabel = input.parentElement;
+            const inputDiv = inputLabel.parentElement;
+            const userAnswer = input.value;
+            
+            const { highlight, textColor } = getUserAnswerFeedback(userAnswer, counter++);
+    
+            inputDiv.classList.add(highlight);
+            inputLabel.classList.add(textColor);
+            inputLabel.classList.remove('text-dark');
+        });
     };
 
-    if (isAGoodScore) {
-        return goodScoreMessage;
-    }
-
-    return badScoreMessage;
-}
-
-const calculateScore = (userAnswer, index) => {
-    if (userAnswer.value === correctAnswers[index]) {
-        score += 1;
-    };
-};
-
-const releaseReloadPage = () => {
-    button.textContent = `Tentar Novamente`;
-    shouldReloadPage = true;
-}
-
-const handleScore = event => {
-    if (shouldReloadPage) {
-        return;
+    const disableAnswersInputs = inputs => {
+        inputs.forEach(input => input.disabled = true);
     };
 
-    event.preventDefault();
+    const getUserAnswers = () => {
+        let userAnswers = [];
 
-    const userAnswers = [
-        form.inputQuestion1,
-        form.inputQuestion2,
-        form.inputQuestion3,
-        form.inputQuestion4,
-        form.inputQuestion5,
-        form.inputQuestion6,
-        form.inputQuestion7,
-        form.inputQuestion8,
-    ];
+        correctAnswers.forEach((_, index) => {
+            const userAnswer = form[`inputQuestion${index + 1}`];
+            userAnswers.push(userAnswer);
+        })
 
-    userAnswers.forEach(calculateScore);
-    userAnswers.forEach(showWrongAnswers);
+        return userAnswers;
+    };
 
-    releaseReloadPage();
-    showPopup(testScore);
-};
+    const calculateScore = userAnswers => {
+        userAnswers.forEach((userAnswer, index) => {
+            const isUserAnswerCorrect = userAnswer.value === correctAnswers[index];
 
-form.addEventListener('submit', handleScore);
-popup.addEventListener('click', closePopup);
+            if (isUserAnswerCorrect) {
+                score += 1;
+            };
+        });
+    };
+
+    const showPopup = getScoreFeedback => {
+        const { title, className } = getScoreFeedback();
+
+        popup.style.display = 'block';
+        popupTitle.textContent = title;
+        popupTitle.classList.add(className);
+
+        let counter = 0;
+
+        const timer = setInterval(() => {
+            if (counter === score) {
+                clearInterval(timer);
+            };
+
+            popupScore.textContent = counter++;
+        }, 100)
+    };
+
+    const getScoreFeedbackMessage = () => {
+        const goodScoreMessage = {
+            title: "Parabéns!",
+            className: 'text-success'
+        };
+
+        const badScoreMessage = {
+            title: 'Que pena!',
+            className: 'text-danger'
+        };
+
+        const isAGoodScore = score >= 4;
+
+        return isAGoodScore ? goodScoreMessage : badScoreMessage;
+    };
+
+    const releaseReloadPage = () => {
+        buttonForm.textContent = `Tentar Novamente`;
+        shouldReloadPage = !shouldReloadPage;
+    };
+
+    const handleScore = event => {
+        if (shouldReloadPage) {
+            return;
+        };
+
+        event.preventDefault();
+
+        const userAnswers = getUserAnswers();
+
+        calculateScore(userAnswers)
+        showPopup(getScoreFeedbackMessage);
+        releaseReloadPage();
+    };
+
+    const closePopup = event => {
+        const classNameOfClickedElement = event.target.classList;
+        const classesNames = ['popup-close', 'popup-wraper'];
+
+        const shouldClosePopup = classesNames.some(
+            className => className === classNameOfClickedElement[0]
+        );
+
+        if (shouldClosePopup) {
+            highlightCheckedInputs(inputs)
+            disableAnswersInputs(inputs);
+
+            popup.style.display = 'none';
+        };
+    };
+
+    form.addEventListener('submit', handleScore);
+    popup.addEventListener('click', closePopup);
+})()
